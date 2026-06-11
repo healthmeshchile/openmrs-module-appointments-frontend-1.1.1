f8dc1e3571b97e33c9a683f750f7e5470f50c03a
juanjortega <juanjortega@gmail.com>
2026-06-11 02:17:00 -0400

    bvn

--- ui/react-components/components/Dropdown/Dropdown.jsx
+++ ui/react-components/components/Dropdown/Dropdown.jsx
@@ -1,12 +1,13 @@
 import {
   disable,
 } from "./Dropdown.module.scss";
+
 import React, { useEffect, useRef } from "react";
 import classNames from "classnames";
 import { PropTypes } from "prop-types";
 import { injectIntl } from "react-intl";
 import { isUndefined } from "lodash";
-import {ComboBox} from "carbon-components-react";
+import { ComboBox } from "carbon-components-react";
 import Title from "../Title/Title.jsx";
 
 const Dropdown = (props) => {
@@ -14,24 +15,30 @@ const Dropdown = (props) => {
     options,
     placeholder,
     onChange,
+    onInputChange,
     isDisabled,
     selectedValue,
     autoFocus,
     isRequired,
   } = props;
+
   const filterItems = data => {
     return data.item.label.toLowerCase().includes(data.inputValue.toLowerCase());
-  }
+  };
+
   const dropdownRef = useRef(null);
+
   useEffect(() => {
     autoFocus && dropdownRef && !isDisabled && dropdownRef.current.focus();
   }, [autoFocus, isDisabled]);
 
   const handleOnChange = (selected) => {
-    onChange(selected.selectedItem)
-  }
+    onChange(selected.selectedItem);
+  };
+
   const isComponentDisabled = () =>
     isUndefined(isDisabled) ? false : isDisabled;
+
   const title = <Title text={placeholder} isRequired={isRequired}/>;
 
   return (
@@ -40,17 +47,18 @@ const Dropdown = (props) => {
       className={classNames(isComponentDisabled() ? disable : "")}
     >
       <ComboBox
-          id={"combo-box"}
-          ref={dropdownRef}
-          items={options}
-          onChange={handleOnChange}
-          itemToString={(item) => (item ? item.label : '')}
-          titleText={title}
-          disabled={isDisabled}
-          style={{ width: '250px' }}
-          shouldFilterItem={filterItems}
-          placeholder={"Choose an option"}
-          selectedItem={selectedValue}
+        id={"combo-box"}
+        ref={dropdownRef}
+        items={options}
+        onChange={handleOnChange}
+        onInputChange={onInputChange}
+        itemToString={(item) => (item ? item.label : '')}
+        titleText={title}
+        disabled={isDisabled}
+        style={{ width: '250px' }}
+        shouldFilterItem={filterItems}
+        placeholder={"Choose an option"}
+        selectedItem={selectedValue}
       />
     </div>
   );
@@ -62,6 +70,7 @@ Dropdown.propTypes = {
   options: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
+  onInputChange: PropTypes.func,
   selectedValue: PropTypes.string,
   isDisabled: PropTypes.bool,
   isRequired: PropTypes.bool,
